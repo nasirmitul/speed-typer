714c27dc6f9aef16538b7101877e0a336cbcbc0f
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-09-15 12:04:04 +0600

    showing the second in integer number

--- script.js
+++ script.js
@@ -136,6 +136,7 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
+  
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
